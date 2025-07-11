3e484bc67efc00207d47201dfcc7b53b547dfd0c
Parth Sharma <05sharmaparth@gmail.com>
2025-07-11 16:48:36 +0530

    song's play and pause and individual song play functionality added

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,18 +1,39 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import DisplayHome from './DisplayHome'
-import DisplayAlbum from './DisplayAlbum'
+import React, { useEffect, useRef } from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import DisplayHome from './DisplayHome';
+import DisplayAlbum from './DisplayAlbum';
+import { albumsData } from '../assets/assets';
 
 const Display = () => {
+  const displayRef = useRef();
+  const location = useLocation();
+  const isAlbum = location.pathname.includes('album');
+  const albumId = isAlbum ? location.pathname.split('/').pop() : '';
+  const albumIndex = Number(albumId);
+
+  const bgColor = (isAlbum && !isNaN(albumIndex) && albumsData[albumIndex])
+    ? albumsData[albumIndex].bgColor
+    : null;
+
+  useEffect(() => {
+    if (isAlbum && bgColor) {
+      displayRef.current.style.background = `linear-gradient(${bgColor}, #121212)`;
+    } else {
+      displayRef.current.style.background = `#121212`;
+    }
+  }, [location.pathname]);
+
   return (
-    <div className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w[75%] lg:ml-0 '>
-        <Routes>
-            <Route path='/' element={<DisplayHome/>} />
-            <Route path='/album/:id' element={<DisplayAlbum/>}/>
-
-        </Routes>
+    <div
+      ref={displayRef}
+      className='w-full m-2 px-6 pt-4 rounded text-white overflow-auto lg:w-[75%] lg:ml-0'
+    >
+      <Routes>
+        <Route path='/' element={<DisplayHome />} />
+        <Route path='/album/:id' element={<DisplayAlbum />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default Display
+export default Display;
--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Navbar from './Navbar'
 import { useParams } from 'react-router-dom'
-import { albumsData, assets } from '../assets/assets';
+import { albumsData, assets, songsData } from '../assets/assets';
+import { PlayerContext } from '../context/PlayerContext';
 
 const DisplayAlbum = () => {
   const {id} = useParams();
   const albumData=albumsData[id];
- 
+const {playWithId} =useContext(PlayerContext);
+  
   return (
     <>
       <Navbar/>
@@ -26,13 +28,27 @@ const DisplayAlbum = () => {
           </p>
         </div>
       </div>
-      <div className='grid grid-cols-3 sm:grid-col-4 mt-10 mb-4 pl-2 text-[#a7a7a7]'>
+      <div className='grid grid-cols-4 sm:grid-col-4 mt-10 mb-4 pl-2 text-[#a7a7a7]'>
       <p><b className='mr-4'>#</b>Title</p>
       <p>Album</p>
       <p className='hidden sm:block'>Date Added</p>
       <img className='m-auto w-4 ' src={assets.clock_icon} alt=''/>
-
       </div>
+      <hr/>
+      {
+        songsData.map((item,index)=>(
+          <div onClick={()=>{playWithId(item.id)}} key={index} className='grid grid-cols-3 sm:grid-cols-4 gap-2 items-center text-[#a7a7a7] p-2 hover:bg-[#ffffff2b] cursor-pointer'>
+<p className='text-white '>
+  <b className='mr-4 text-[#a7a7a7]'>{index+1}</b>
+  <img src={item.image} className='inline w-10 mr-5 ' alt=''/>
+  {item.name}
+</p>
+<p className='text-[15px]'>{albumData.name}</p>
+<p className='text-[15px] hidden sm:block'>5 days ago</p>
+<p className='text-[15px] text-center'>{item.duration}</p>
+          </div>
+        ))
+      }
     </>
   )
 }
