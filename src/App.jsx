ceee0160eb5cbe4648424d3fb0a8045d25788728
Parth Sharma <05sharmaparth@gmail.com>
2025-07-11 15:22:38 +0530

    album and song added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Sidebar from './components/Sidebar'
 import Player from './components/Player'
+import Display from './components/Display'
+import DisplayHome from './components/DisplayHome'
 
 const App = () => {
   return (
     <div className='h-screen bg-black'>
       <div className='h-[90%] flex'>
          <Sidebar/>
+         <Display/>
       </div>
       <Player/>
     </div>
